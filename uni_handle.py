b11365bbcc88e060e83ad6604b528b7f82513f6c
21803086 <21803086@student.curtin.edu.au>
2025-09-30 12:21:58 +0800

    Updated file docstring

--- uni_handle.py
+++ uni_handle.py
@@ -1,8 +1,8 @@
 """
 Title: Uni(versal) Handle(r) / UniHandle
-Author: Kathryn Phillips
+Author: Kathryn M Phillips (Berbless)
 Created: 25.03.25 | DD.MM.YY
-Version: 0.6.0 (28.09.25) | DD.MM.YY
+Updated: 30.09.25 | DD.MM.YY
 Description: Simple universal args and menu tool for executing python functions.
 """
 
